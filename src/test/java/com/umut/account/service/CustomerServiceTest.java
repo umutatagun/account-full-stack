a3a8b73b93e91a3be13ea75a062dfa3908416380
umutatagun <umutatgn@gmail.com>
2023-02-14 22:52:30 +0300

    customer service test

--- src/test/java/com/umut/account/service/CustomerServiceTest.java
+++ src/test/java/com/umut/account/service/CustomerServiceTest.java
@@ -1,7 +1,63 @@
 package com.umut.account.service;
 
+import com.umut.account.dto.CustomerDto;
+import com.umut.account.dto.converter.CustomerDtoConverter;
+import com.umut.account.exception.CustomerNotFoundException;
+import com.umut.account.model.Customer;
+import com.umut.account.repository.CustomerRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerServiceTest {
 
+    private CustomerService service;
+    private CustomerRepository customerRepository;
+    private CustomerDtoConverter converter;
+
+    @BeforeEach
+    public void setUp() {
+        customerRepository = mock(CustomerRepository.class);
+        converter = mock(CustomerDtoConverter.class);
+        service = new CustomerService(customerRepository, converter);
+    }
+
+    @Test
+    public void testFindByCustomerId_whenCustomerIdExists_shouldReturnCustomer() {
+        Customer customer = new Customer("id", "name", "surname", Set.of());
+        when(customerRepository.findById("id")).thenReturn(Optional.of(customer));
+
+        Customer result = service.findCustomerById("id");
+
+        assertEquals(result, customer);
+    }
+
+    @Test
+    public void testFindByCustomerId_whenCustomerIdDoesNotExists_shouldThrowCustomerNotFoundException() {
+        when(customerRepository.findById("id")).thenReturn(Optional.empty());
+
+        assertThrows(CustomerNotFoundException.class, () -> service.findCustomerById("id"));
+    }
+
+    @Test
+    public void testGetCustomerById_whenCustomerIdExists_shouldReturnCustomer() {
+        Customer customer = new Customer("id", "name", "surname", Set.of());
+        CustomerDto customerDto = new CustomerDto("id", "name", "surname", Set.of());
+
+        when(customerRepository.findById("id")).thenReturn(Optional.of(customer));
+        when(converter.convertToCustomerDto(customer)).thenReturn(customerDto);
+
+        CustomerDto result = service.getCustomerById("id");
+
+        assertEquals(result, customerDto);
+    }
 }
